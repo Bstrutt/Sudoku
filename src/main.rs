c0a87de6a0921127e9ad7806b6c32fdf1c635ca9
Bstrutt <bryce1@iastate.edu>
2020-01-30 22:16:24 -0600

    It's finished. It solves sudoku, recursively, depth first search. I don't know why my linking is screwing up. Probably a vscode error

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@ mod display;
 mod checks;
 mod solver;
 
+
 use display :: print_board;
 use solver::solve;
 fn main() {
     let mut state = [[0; 9]; 9];
-    state[0] = [0,0,0, 0,0,0, 2,0,0];
-    state[1] = [0,8,0, 0,0,7, 0,9,0];
-    state[2] = [6,0,2, 0,0,0, 5,0,0];
-    state[3] = [0,7,0, 0,6,0, 0,0,0];
-    state[4] = [0,0,0, 9,0,1, 0,0,0];
-    state[5] = [0,0,0, 0,2,0, 0,4,0];
-    state[6] = [0,0,5, 0,0,0, 6,0,3];
-    state[7] = [0,9,0, 4,0,0, 0,7,0];
-    state[8] = [0,0,6, 0,0,0, 0,0,0];
+    state[0] = [5,4,0, 0,2,0, 8,0,6];
+    state[1] = [0,1,9, 0,0,7, 0,0,3];
+    state[2] = [0,0,0, 3,0,0, 2,1,0];
+    state[3] = [9,0,0, 4,0,5, 0,2,0];
+    state[4] = [0,0,0, 0,0,0, 6,0,4];
+    state[5] = [6,0,4, 0,3,2, 0,8,0];
+    state[6] = [0,6,0, 0,0,0, 1,9,0];
+    state[7] = [4,0,2, 0,0,9, 0,0,5];
+    state[8] = [0,9,0, 0,7,0, 4,0,2];
    
     solve(&mut state);
+    print!("\n\n\n");
     print_board(&state);
 
     if checks::check_legal(&state) {
--- src/solver.rs
+++ src/solver.rs
@@ -1,6 +1,5 @@
 use crate::checks;
 use crate::display;
-
 pub fn solve(state: &mut [[u32; 9]; 9]) ->() {
     solve_cell(state, 0, 0);
 }
@@ -11,8 +10,9 @@ fn solve_cell(state: &mut [[u32; 9]; 9], x: usize, y: usize) -> bool{
     if state[x][y]!=0 { 
         return solve_next(state, x, y);
     } else { // if there isnt already something in the cell
-        for i in 0..9{
+        for i in 1..10{
             state[x][y] = i;
+            display::print_board(state);
             if checks::check_legal(&state) { // check new state for legality
                 if solve_next(state, x, y) {
                     return true;   
@@ -23,21 +23,22 @@ fn solve_cell(state: &mut [[u32; 9]; 9], x: usize, y: usize) -> bool{
                 continue; //if not legal try the next number
             }
         }
+       state[x][y] = 0;
         return false;
     }
 }
 fn solve_next(state: &mut [[u32; 9]; 9], x: usize, y: usize) -> bool{
-    if y==8{
-        if x==8{
+    if x==8{
+        if y==8{
             return true;
         } else {
-            return solve_cell(state, x+1, y);
+            return solve_cell(state, x, y+1);
         }
     } else {
-        if x==8{
-            return solve_cell(state, 0, y+1);
+        if y==8{
+            return solve_cell(state, x+1, 0);
         } else {
-            return solve_cell(state, x+1, y);
+            return solve_cell(state, x, y+1);
         }
     } 
 }
